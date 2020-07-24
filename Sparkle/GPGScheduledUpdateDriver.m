4e5f851def221d9010f8e82cd0319b35fe4c0a95
Mento <Mento@gpgtools.org>
2020-07-24 09:56:03 +0200

    Do not show notification and dialog at the same time.

--- Sparkle/GPGScheduledUpdateDriver.m
+++ Sparkle/GPGScheduledUpdateDriver.m
@@ -17,11 +17,19 @@
 static NSString *const GPGAppcastElementDisableNotification = @"sparkle:gpgtoolsDisableNotification";
 static NSString *const GPGAppcastElementInfoOnlyNotification = @"sparkle:gpgtoolsInfoOnlyNotification";
 
+typedef NS_ENUM(NSInteger, GPGUserNotificationStyle) {
+    GPGUserNotificationStyleNone = 0,
+    GPGUserNotificationStyleBanner,
+    GPGUserNotificationStyleAlert,
+};
+
 
 
 @interface GPGScheduledUpdateDriver () <NSUserNotificationCenterDelegate>
 @property (nonatomic) BOOL notificationDismissed;
 @property (nonatomic) BOOL shouldDisableKeyboardShortcutForInstallButton;
+@property (nonatomic, strong) NSUserNotification *updateNotification;
+@property (getter=isInterruptible) BOOL interruptible;
 @end
 
 @interface SUUIBasedUpdateDriver (MakeVisiable)
@@ -57,6 +65,8 @@ static NSString *localized(NSString *key) {
 
 @implementation GPGScheduledUpdateDriver
 @synthesize notificationDismissed, shouldDisableKeyboardShortcutForInstallButton;
+@synthesize updateNotification;
+@synthesize interruptible;
 
 - (void)didFindValidUpdate {
     if (@available(macos 10.10, *)) {
@@ -87,6 +97,8 @@ static NSString *localized(NSString *key) {
 //      NSUserNotificationAction *installAction = [NSUserNotificationAction actionWithIdentifier:@"install" title:localized(@"NotificationInstall")];
 //      notification.additionalActions = @[installAction];
         
+        self.updateNotification = notification;
+        
         NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
         center.delegate = self;
         [center deliverNotification:notification];
@@ -99,66 +111,69 @@ static NSString *localized(NSString *key) {
     }
 }
 
+- (void)abortUpdate {
+    [self dismissNotification:self.updateNotification];
+    [super abortUpdate];
+}
+
 - (void)dismissNotification:(NSUserNotification *)notification {
     [[NSUserNotificationCenter defaultUserNotificationCenter] removeDeliveredNotification:notification];
     self.notificationDismissed = YES;
 }
 
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)notification {
-    NSString *identifier = notification.identifier;
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        BOOL notificationBlocked = YES;
-        BOOL notificationStillPresent;
-        NSInteger isBlockedCounter = 10;
+
+    // In case the user has disabled notifications for GPGSuite_Updater,
+    // show the traditional dialog.
+    GPGUserNotificationStyle displayStyle;
+    @try {
+        NSNumber *displayStyleValue = [notification valueForKey:@"_displayStyle"]; // Private API. Better do this in @try @catch.
+        displayStyle = displayStyleValue.integerValue;
+    } @catch (__unused NSException *exception) {
+        displayStyle = GPGUserNotificationStyleNone;
+    }
+
+    if (displayStyle == GPGUserNotificationStyleNone) {
+        [self dismissNotification:notification];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [super didFindValidUpdate];
+        });
+    } else {
+        // Set interruptible to YES, so a manual updated check if possible, even if a update notification is on screen.
+        self.interruptible = YES;
+
         
-        // This loop waits, until the notificaion is dismissed.
-        do {
-            [NSThread sleepForTimeInterval:1];
-            
-            // Check if the notification is still on screen.
-            notificationStillPresent = NO;
-            for (NSUserNotification *aNotification in center.deliveredNotifications) {
-                if ([aNotification.identifier isEqualToString:identifier]) {
-                    notificationStillPresent = YES;
-                    break;
-                }
-            }
-            
-            
-            // This code checks if the user blocked notifications. In that case the normal update dialog is shown.
-            if (isBlockedCounter >= 0 && !self.notificationDismissed && notificationStillPresent) {
-                isBlockedCounter--;
-                if (isBlockedCounter == 0) {
-                    @try {
-                        NSArray *presentedAlerts = [center valueForKey:@"_presentedAlerts"];
-                        for (NSUserNotification *aNotification in presentedAlerts) {
-                            if ([aNotification.identifier isEqualToString:identifier]) {
-                                notificationBlocked = NO;
-                                break;
-                            }
-                        }
-                    } @catch (__unused NSException *exception) {
+        // TODO: Instead of waiting in a loop, close the updater and re-open it, when the user clicks on a notification.
+        
+        // Wait until the notification is gone, so the user has a chance to click on it, to trigger the update.
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            NSString *identifier = notification.identifier;
+
+            BOOL notificationStillPresent;
+            do {
+                // Check from time to time, if the notification is gone.
+                [NSThread sleepForTimeInterval: 10];
+                
+                notificationStillPresent = NO;
+                for (NSUserNotification *aNotification in center.deliveredNotifications) {
+                    if ([aNotification.identifier isEqualToString:identifier]) {
+                        // The notification is still there, so an user could click on it to trigger the update process.
+                        notificationStillPresent = YES;
+                        break;
                     }
                 }
-                if (isBlockedCounter == -1 && notificationBlocked) {
-                    // The user has blocked notifications from GPGSuite.
-                    // Show the standard update dialog.
-                    [self dismissNotification:notification];
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        [super didFindValidUpdate];
-                    });
-                }
-            }
+            } while (notificationStillPresent);
             
-        } while (!self.notificationDismissed && notificationStillPresent);
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (!self.notificationDismissed) {
-                [self abortUpdate];
-            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (!self.notificationDismissed) {
+                    // The user did not click on the notificaiton.
+                    // Cancel the update, because it can not be triggered anymore.
+                    [self abortUpdate];
+                }
+            });
         });
-    });
+    }
     
 }
 
@@ -168,6 +183,7 @@ static NSString *localized(NSString *key) {
         case NSUserNotificationActivationTypeContentsClicked:
             // The user clicked on the notification.
             [self dismissNotification:notification];
+            self.interruptible = NO;
             [super didFindValidUpdate];
             break;
         case NSUserNotificationActivationTypeActionButtonClicked:
@@ -176,6 +192,7 @@ static NSString *localized(NSString *key) {
             if (!self.updateItem.isInformationOnlyUpdate && ![self.updateItem.propertiesDictionary[SUAppcastElementTags] containsObject:GPGAppcastElementInfoOnlyNotification]) {
                 self.automaticallyInstallUpdates = YES;
             }
+            self.interruptible = NO;
             [super didFindValidUpdate];
             break;
 //      case NSUserNotificationActivationTypeAdditionalActionClicked: {
